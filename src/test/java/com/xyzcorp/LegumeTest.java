d6a3c423a8c15f6d7d208032a9ad17b51999e4eb
Brendan Byrne <b.byrne@cgi.com>
2022-02-02 14:17:23 -0500

    Moved legume test to its own file

--- src/test/java/com/xyzcorp/LegumeTest.java
+++ src/test/java/com/xyzcorp/LegumeTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class FruitTest {
+public class LegumeTest {
     @Test
-    public void testGetFruits() {
+    public void testGetLegumes() {
         given()
                 .relaxedHTTPSValidation()
                 .accept(ContentType.JSON)
                 .when()
-                .get("https://staging.tiered-planet.net/mild-temper/fruits")
+                .get("https://staging.tiered-planet.net/mild-temper/legumes")
                 .then()
                 .assertThat()
-                .body("[0].description", equalTo("Winter fruit"));
+                .body("[1].name", equalTo("Zucchini"));
     }
 }
