9f67eea5e80c144e80d91eb1a5b1ee013f447112
Brendan Byrne <b.byrne@cgi.com>
2022-02-02 15:28:43 -0500

    Added fruit post

--- src/test/java/com/xyzcorp/FruitTest.java
+++ src/test/java/com/xyzcorp/FruitTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.json.JSONObject;
+
 public class FruitTest {
     @Test
     public void testGetFruits() {
@@ -17,5 +19,24 @@ public class FruitTest {
                 .then()
                 .assertThat()
                 .body("[0].description", equalTo("Winter fruit"));
+    }
+
+    @Test
+    public void testPostNewFruit () {
+
+        JSONObject bananaObject = new JSONObject()
+            .put("description", "a delicious treat")
+            .put("name", "banana");
+
+        given()
+            .relaxedHTTPSValidation()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .body(bananaObject.toString())
+            .when()
+            .post("https://staging.tiered-planet.net/mild-temper/fruits")
+            .then()
+            .assertThat()
+            .statusCode(200);
     }
 }
